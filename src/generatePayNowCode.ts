f20286215a1c8c4848912293825e4ba82c31938e
limtingzhou <tingzhou@policypal.com>
2021-04-04 15:29:32 +0800

    convert payment amount to string with 2dp, allow for mobile and uen payments

--- src/generatePayNowCode.ts
+++ src/generatePayNowCode.ts
@@ -2,15 +2,18 @@ import getStrWithOptions from './strWithOptions.js'
 import crc16CheckSum from './crc16CheckSum.js'
 
 export interface PayNowOptions {
-  amount: number,
-  uen: string,
-  refNumber: string,
-  editable: number,
+  paymentAmount: number,
+  recipientIdentifierType: 'MOBILE' | 'UEN',
+  recipientIdentifier: string,
+  description: string,
+  editable: boolean,
+  expiryDate?: string,
 }
 
 function generatePayNowCode(opts: PayNowOptions) {
   const strWithOptions = getStrWithOptions(opts);
-  const checkSum = crc16CheckSum(`${strWithOptions}6304}`);
+  const checkSum = crc16CheckSum(`${strWithOptions}6304`);
+
   return `${strWithOptions}6304${checkSum}`
 }
 
--- src/strWithOptions.ts
+++ src/strWithOptions.ts
@@ -1,35 +1,46 @@
 import padLeft from './padLeft.js'
-import {PayNowOptions} from './generatePayNowCode'
+import type {PayNowOptions} from './generatePayNowCode.js'
+
+function getFormattedPaymentAmount(opts: PayNowOptions) {
+const paymentAmount = opts.paymentAmount.toLocaleString("en-US", { maximumFractionDigits: 2, minimumFractionDigits: 2 });
+  return paymentAmount
+}
+
+function getRecipientAccountInfoTemplate(opts: PayNowOptions) {
+  const recipientIdentifierValue = opts.recipientIdentifierType === 'MOBILE' ? '0' : '2';
+  const template = [
+    {id: '00', value: 'SG.PAYNOW'},
+    {id: '01', value: recipientIdentifierValue },
+    {id: '02', value: opts.recipientIdentifier},
+    {id: '03', value: opts.editable ? '1' : '0'}, // 1 = Payment amount is editable, 0 = Not Editable
+  ]
+  if (opts.expiryDate) {
+    template.push({ id: "04", value: opts.expiryDate });
+  }
+  return template;
+}
 
 
 function generateStrWithOptions(opts: PayNowOptions) {
   const p = [
     {id: '00', value: '01'}, // ID 00: Payload Format Indicator (Fixed to '01')
-    {id: '01', value: '12'}, // ID 01: Point of Initiation Method 11: static, 12: dynamic
+    {id: '01', value: '11'}, // ID 01: Point of Initiation Method 11: static, 12: dynamic
     {
       id: '26',
-      // ID 26: Merchant Account Info Template
-      value: [
-        {id: '00', value: 'SG.PAYNOW'},
-        {id: '01', value: '2'}, // 0 for mobile, 2 for UEN. 1 is not used.
-        {id: '02', value: opts.uen}, // PayNow UEN (Company Unique Entity Number)
-        {id: '03', value: opts.editable.toString()}, // 1 = Payment amount is editable, 0 = Not Editable
-        // { id: "04", value: opts.expiry },
-      ], // Expiry date (YYYYMMDD)
+      value: getRecipientAccountInfoTemplate(opts)
     },
     {id: '52', value: '0000'}, // ID 52: Merchant Category Code (not used)
     {id: '53', value: '702'}, // ID 53: Currency. SGD is 702
-    {id: '54', value: opts.amount.toString()}, // ID 54: Transaction Amount
+    {id: '54', value: getFormattedPaymentAmount(opts)}, // ID 54: Transaction Amount
     {id: '58', value: 'SG'}, // ID 58: 2-letter Country Code (SG)
-    {id: '59', value: 'COMPANY NAME'}, // ID 59: Company Name
+    {id: '59', value: 'NA'}, // ID 59: Company Name
     {id: '60', value: 'Singapore'}, // ID 60: Merchant City
     {
-      id: '62',
+      id: '62', // ID 62: Additional data fields
       value: [
         {
-          // ID 62: Additional data fields
           id: '01',
-          value: opts.refNumber, // ID 01: Bill Number
+          value: opts.description, // ID 01: Bill Number
         },
       ],
     },
@@ -37,27 +48,18 @@ function generateStrWithOptions(opts: PayNowOptions) {
 
   let str = p.reduce((final, current) => {
     if (Array.isArray(current.value)) {
-      //nest loop
       current.value = current.value.reduce((f, c) => {
-        // before edit
-        // f += c.id + c.value.length.toString().padLeft(2) + c.value;
-
-        // after edit
         f += c.id + padLeft(2, c.value.length.toString()) + c.value;
         return f;
       }, '');
     }
 
-    // before edit
-    // final +=
-    //   current.id + current.value.length.toString().padLeft(2) + current.value;
-
-    // after edit
     final +=
       current.id + padLeft(2, current.value.length.toString()) + current.value;
     return final;
   }, '');
+
   return str;
 }
 
-export default generateStrWithOptions
+export default generateStrWithOptions;
